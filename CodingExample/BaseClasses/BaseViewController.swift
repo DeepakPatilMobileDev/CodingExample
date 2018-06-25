8f34ae671604d0a5c9af5d6376ec0fbdd8d5ba96
Deepak P. Patil <dpatil@ios-dpatil.local>
2018-06-25 10:34:08 +0530

    Added Base View Controller Class

---
+++ CodingExample/BaseClasses/BaseViewController.swift
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.swift
+//  CodingExample
+//
+//  Created by Deepak P. Patil on 25/06/18.
+//  Copyright © 2018 Deepak P. Patil. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class  BaseViewController : UIViewController {
+    
+}
